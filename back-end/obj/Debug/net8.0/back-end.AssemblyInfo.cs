e9d23c1780d1b315cef5957d5d90c0b8fd4e37e0
Aigaras Vaitkus <aigarasvaitkus@gmail.com>
2025-05-24 16:10:18 +0000

    Finished implementing post in backend

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f126abb5526ddb32ef82f9d2aa3c6b0b3701cea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127bbff5dea9c55a3c577e6cb0314e40c70c59f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
