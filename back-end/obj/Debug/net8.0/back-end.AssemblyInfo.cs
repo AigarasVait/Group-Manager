fd21eb6d95fdfa755353d562337f14ebc40c00d6
root <root@DESKTOP-9R539AF.>
2025-05-24 12:34:18 +0000

    Merging assembly info

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9538db79d7ed05fe35d5527d68fe95b8f19a5ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec94e1edba506ef413664dd6eb487d6b407e03a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
